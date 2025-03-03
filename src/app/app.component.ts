ff7bd33cf10fe3b2fd53eca9afd20dc950610094
thiscomputer <thiscomputer@Mac-mini.local>
2025-03-02 21:38:25 -0700

    Brand new angular project, that just has form fields. first commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInput } from '@angular/material/input';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatInput,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [provideNativeDateAdapter()],
 })
 export class AppComponent {
-  title = 'angular-rxjs';
+  formGroup: FormGroup;
+
+  constructor(private data: FormBuilder) {
+    this.formGroup = this.data.group({
+      numberOfDays: ['', Validators.required],
+      date: ['', Validators.required],
+    });
+  }
 }
